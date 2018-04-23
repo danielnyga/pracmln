8d3f6bbc312308a8a95124883264c55265bf1ab1
Kaivalya Rawal <kaivalyar@users.noreply.github.com>
2018-04-23 17:49:50 +0530

    fixing name error

--- python3/pracmln/mln/learning/multidb.py
+++ python3/pracmln/mln/learning/multidb.py
@@ -21,6 +21,7 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 from dnutils import logs, ProgressBar, out, first
+from first import first
 
 from .common import AbstractLearner
 import sys
