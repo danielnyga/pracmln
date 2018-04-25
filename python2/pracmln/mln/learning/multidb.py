b3f0d6cc52d10785af80e5da683f4a0d26d277a0
Daniel Nyga <nyga@cs.uni-bremen.de>
2018-04-25 11:08:54 +0200

    fixed import

--- python2/pracmln/mln/learning/multidb.py
+++ python2/pracmln/mln/learning/multidb.py
@@ -20,7 +20,7 @@
 # CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-from dnutils import logs, ProgressBar
+from dnutils import logs, ProgressBar, first
 
 from common import AbstractLearner
 import sys
