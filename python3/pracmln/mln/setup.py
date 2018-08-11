a4f156a4cd8c0321b6a4b4bd3ad692f10ad17fbb
Kaivalya Rawal <kaivalyarawal45@gmail.com>
2018-08-11 18:03:44 +0800

    revert temporary edits

--- python3/pracmln/mln/setup.py
+++ python3/pracmln/mln/setup.py
@@ -3,5 +3,5 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules=cythonize("*.pyx", compiler_directives={'profile': True})
+    ext_modules=cythonize("*.pyx")#, compiler_directives={'profile': True})
 )
